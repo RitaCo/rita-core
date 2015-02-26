b3d4e91e93700c7788e7d21056ba381a15d643bc
zoghal <saleh.souzanchi@gmail.com>
2015-02-26 17:46:40 +0330

    add PagesController and importent actions for core

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Rita\Core\Controller;
+
+use Rita\Core\Controller\AppController;
+
+class PagesController extends AppController
+{
+    
+    
+    public function outOfService(){
+        
+    }
+}
